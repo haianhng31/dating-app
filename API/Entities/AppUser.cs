41f003a55bb898831b2a850d4f5c95c215466600
Hennessy Nguyen <anguyen137@fordham.edu>
2024-06-03 14:56:22 +0700

    migrations to add password to user entity

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -5,6 +5,8 @@ public class AppUser
     // EF needs to be public 
     public int Id { get; set; }
     public string UserName { get; set; }
+    public byte[] PasswordHash { get; set; }
+    public byte[] PasswordSalt { get; set; }
 
 
 }
