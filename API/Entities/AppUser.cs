062c8f41ac8d04ea393f993bf190ab299af7536a
Hennessy Nguyen <anguyen137@fordham.edu>
2024-06-03 16:23:27 +0700

    create DTO folder and files

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,9 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.Entities; 
 
 public class AppUser 
 { 
     // EF needs to be public 
     public int Id { get; set; }
+    [Required]
     public string UserName { get; set; }
     public byte[] PasswordHash { get; set; }
     public byte[] PasswordSalt { get; set; }
