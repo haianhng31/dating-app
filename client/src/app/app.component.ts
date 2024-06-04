031180bdfa62dbce666bb9213602b1c9114617ea
Hennessy Nguyen <anguyen137@fordham.edu>
2024-06-04 14:04:57 +0700

    handle setCurrentUser() in app.component when refresh page

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from './services/account.service';
+import { User } from './models/users';
 
 @Component({
   selector: 'app-root',
@@ -13,16 +15,27 @@ export class AppComponent implements OnInit {
   title: string = 'hello';
   users: any;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private accountService: AccountService) {}
   // The constructor is normally considered too early to go and fetch data from the API 
   // -> Implement a lifecycle event inside this component
 
   ngOnInit(): void {
+    this.getUsers();
+    this.setCurrentUser();
+  }
+
+  getUsers() {
     this.http.get('https://localhost:5001/api/users').subscribe({
       next: response => this.users = response,
       error: error => console.error('Error: ', error),
       complete: () => console.log('Done')
     })
-    // this returns an Observable
+  }
+
+  setCurrentUser() {
+    const userString = localStorage.getItem('user');
+    if (!userString) return;
+    const user: User = JSON.parse(userString);
+    this.accountService.setCurrentUser(user);
   }
 }
