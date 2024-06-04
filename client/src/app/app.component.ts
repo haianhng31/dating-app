fc183db9ec852c8f858218b09bb836297d06157c
Hennessy Nguyen <anguyen137@fordham.edu>
2024-06-04 14:48:56 +0700

    add communication between home and register component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from './services/account.service';
 import { User } from './models/users';
@@ -15,21 +14,12 @@ export class AppComponent implements OnInit {
   title: string = 'hello';
   users: any;
 
-  constructor(private http: HttpClient, private accountService: AccountService) {}
+  constructor(private accountService: AccountService) {}
   // The constructor is normally considered too early to go and fetch data from the API 
   // -> Implement a lifecycle event inside this component
 
   ngOnInit(): void {
-    this.getUsers();
     this.setCurrentUser();
-  }
-
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.error('Error: ', error),
-      complete: () => console.log('Done')
-    })
   }
 
   setCurrentUser() {
---
+++ client/src/app/home/home.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  registerMode: boolean = false;
+  users: any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  registerToggle() {
+    this.registerMode =!this.registerMode;
+  }
+
+  getUsers() {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: response => this.users = response,
+      error: error => console.error('Error: ', error),
+      complete: () => console.log('Done')
+    }) 
+    console.log(this.users);
+  }
+
+  cancelRegisterMode(event: boolean) {
+    this.registerMode = event;
+  }
+
+}
