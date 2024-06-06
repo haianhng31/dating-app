afa7c4b15f0b172597645525449d7ade4d6121bd
Hennessy Nguyen <anguyen137@fordham.edu>
2024-06-06 13:38:18 +0700

    create NotFound and ServerError

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -24,7 +24,7 @@ export class NavComponent implements OnInit {
   login() {
     this.accountService.login(this.model).subscribe({
       next: () => this.router.navigateByUrl("/members"),
-      error: error => this.toastr.error(error.error)
+      // error: error => this.toastr.error(error.error) // this is handled inside interceptor now 
     })
   }
 
