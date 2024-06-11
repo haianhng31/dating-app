3f20e19f59e5ac2904dd99101f16a9cf8ac2818f
Hennessy Nguyen <anguyen137@fordham.edu>
2024-06-11 17:34:36 +0700

    continue the Program.cs

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,7 @@
+using API.Data;
 using API.Extensions;
 using API.Middleware;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,6 +22,7 @@ var app = builder.Build();
 app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
+
 app.UseHttpsRedirection();
 app.UseRouting();
 
@@ -30,6 +33,15 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+// using var scope = app.Services.CreateScope();
+// var services = scope.ServiceProvider;
+// try {
+//     var context = services.GetRequiredService<DataContext>();
+//     await context.Database.MigrateAsync();
+//     await Seed.SeedUser(context);
+// } catch (Exception ex){
+//     var logger = 
+// }
 
 app.Run();
 
